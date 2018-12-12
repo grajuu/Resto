a7161d19e40fbee5df4b3f470fff4a37570073cb
MGR <mardubudi@Govinds-MacBook-Air.local>
2018-12-12 12:59:06 +0530

    Config client
    Resto Config Client

---
+++ RestoConfig-clint/src/main/java/com/tis/mgr/resto/config/ClientConfigMsg.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.tis.mgr.resto.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mardubudi
+ *
+ */
+@RefreshScope
+@RequestMapping("/rest")
+@RestController
+public class ClientConfigMsg {
+	
+	@Value("${message : Default message }")
+	private String message;
+	
+	@GetMapping("/message")
+	public String message() {
+		
+		return message;
+	}
+
+}
